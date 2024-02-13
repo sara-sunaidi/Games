cf35191ec376442f5a47cb25baa6ffd9b3a9f7d2
Sara Alsunaidi <sara.alsunaidi58@gmail.com>
2024-02-13 11:33:38 +0300

    Add project files.

---
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -0,0 +1,21 @@
+namespace GameEngine.Tests
+{
+    public class PlayerCharacterShould
+    {
+        [Fact]
+        public void BeInexperiencedWhenNew()
+        {
+            var sut = new PlayerCharacter();
+            Assert.True( sut.IsNoob);
+        }
+
+        [Fact]
+        public void CalculateFullName()
+        {
+            var sut = new PlayerCharacter();
+            sut.FirstName = "11feb";
+            sut.LastName = "2024";
+            Assert.Equal("11feb2024",sut.FullName);
+        }
+    }
+}
